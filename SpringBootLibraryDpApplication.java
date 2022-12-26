0c1bfa5ca19ec78d9fa2f3fbbcbced7276e3a3e4
SriKoumudi <74343863+SriKoumudi@users.noreply.github.com>
2022-12-26 17:51:17 +0530

    Add files via upload

---
+++ SpringBootLibraryDpApplication.java
@@ -0,0 +1,14 @@
+package com.greatlearning.library.springbootlibrarydp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLibraryDpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLibraryDpApplication.class, args);
+		
+	}
+
+}
